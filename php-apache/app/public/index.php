78cb288be0432dd9305fa3a9dcb11df9ec3fdb72
yuuya-hataoka <yuuya.a.28@gmail.com>
2022-06-20 23:23:43 +0900

    '穴埋め　終了'

--- php-apache/app/public/index.php
+++ php-apache/app/public/index.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $dbh = new PDO('mysql:dbname=[ 6 ];host=[ 7 ];unix_socket=/tmp/mysql.sock', 'root', '[ 8 ]');
+    $dbh = new PDO('mysql:dbname=docker_review;host=review-mysql;unix_socket=/tmp/mysql.sock', 'root', 'e2QNqCUd');
     echo '<h1 style="color:yellow">Congratulations</h1>';
 } catch (PDOException $e) {
     echo '<h1 style="color:red">Failed<br><p>' . $e->getMessage() . '</p>';
